291cfc930f089d61a30f0e782587662c31577535
mahipurohit1 <mahipurohit174@gmail.com>
2023-02-10 17:51:17 +0530

    classes section start

--- 03 next-gen-javascript-typescript/src/app.ts
+++ 03 next-gen-javascript-typescript/src/app.ts
@@ -1,1 +1,13 @@
-// Code goes here!
+class Department {
+  firstName: string;
+  constructor(n: string) {
+    this.firstName = n;
+  }
+  print(this: Department) {
+    console.log(this.firstName);
+  }
+}
+
+const mahi = new Department("mahi");
+mahi.print();
+console.log(mahi);
